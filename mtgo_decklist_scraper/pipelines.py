5f9821c27d1e8d6dd06f3c24df78a2170bdba3c4
Kevin Brenneman <kalvin66rocks@gmail.com>
2020-10-29 22:37:46 -0400

    Update pipelines.py
    Update parser to have a more friendly format for further analysis

--- mtgo_decklist_scraper/pipelines.py
+++ mtgo_decklist_scraper/pipelines.py
@@ -10,20 +10,38 @@ class MtgoDecklistScraperPipeline(object):
             output_path.unlink()
 
         self.__f = output_path.open('w')
-        self.__write_row(['event', 'deck_id', 'player', 'is_sideboard', 'number', 'card_name', 'card_type'])
+        # self.__write_row(['event                    ', 'deck_id', 'player', 'is_sideboard', 'number', 'card_name', 'card_type'])
 
     def process_item(self, item, spider):
+        row = [
+                str(item['deck_id']), item['player']
+        ]
+        self.__write_row(row)
+        row = [
+                'Main Deck'
+        ]
+        self.__write_row(row)
         for c in item['mainboard'].card_list:
             row = [
-                spider.identifier, str(item['deck_id']), item['player'], 'False', str(c.number), c.name, c.card_type,
+                str(c.number), c.name
             ]
             self.__write_row(row)
+
+        row = [
+                'Sideboard'
+        ]
+        self.__write_row(row)
 
         for c in item['sideboard'].card_list:
             row = [
-                spider.identifier, str(item['deck_id']), item['player'], 'True', str(c.number), c.name, str(c.card_type),
+                str(c.number), c.name
             ]
             self.__write_row(row)
+
+        row = [
+                ' '
+        ]
+        self.__write_row(row)
 
         return item
 
